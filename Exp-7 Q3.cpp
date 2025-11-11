25bae6eb0b2191d78e039bd7318a70581f165b63
Kinjal070608 <kinjalkolekar@gmail.com>
2025-11-12 00:05:26 +0530

    Exp-7 Q3.cpp

---
+++ Exp-7 Q3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+class Number{
+    private:
+    int x;
+    public:
+    void accept() {
+        cout<<"Enter a number:";
+        cin>>x;
+    }
+    void operator -() {
+        x = -x;
+    }
+    void display() {
+        cout<<"Negated number: "<<x<<endl;
+    }
+};
+int main() {
+    Number n1;
+    n1.accept();
+    -n1;
+    n1.display();
+    return 0;
+}
